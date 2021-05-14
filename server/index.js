9965e506f9abcf520608faa174d9c5260703013c
MarieliseDaoud <201710411@ua.edu.lb>
2021-05-14 19:03:44 +0300

    server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://MarieliseDaoud:pastalucky@cluster0.k6fst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://MarieliseDaoud:pastalucky@cluster0.k6fst.mongodb.net/mySecondtDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
